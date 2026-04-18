c462ef7045e880d955ff3ebaa5474a04cd82a481
doremyswt <doremyswt@users.noreply.github.com>
2026-04-18 02:25:44 +0000

    deploy: 0ebbf84c79c37dbc818f417944ef3da093cec25b

---
+++ _app/immutable/chunks/D5zmhl9T.js
@@ -0,0 +1,1 @@
+import{f as r,b as t,h as i}from"./BlmnAMwX.js";import{g as o}from"./DUpt28KJ.js";import"./9-PsTJc7.js";let u=({type:m,originator:a})=>({required_width:200,required_height:101,menu:[[{name:"Open",action:()=>{let e=o(i)[r];t.set({path:"./programs/my_computer.svelte",fs_item:e})},font:"bold"}],[{name:"Properties",action:()=>{}}]]});export{u as make};
---
+++ _app/immutable/nodes/2.BZxQIhEZ.js
@@ -0,0 +1,1 @@
+import{_ as m}from"../chunks/BlmnAMwX.js";export{m as component};
